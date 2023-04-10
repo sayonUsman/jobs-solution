0aeeab94f0f9ceda0aa40b7d8a8be7ac6b3596ad
Sayon Usman <sayonusman.ma@gmail.com>
2023-04-10 15:50:20 +0300

    Added route

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,21 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Main from "./components/Layout/Main";
+import Home from "./components/Home/Home";
+import Statistics from "./components/Statistics/Statistics";
+import AppliedJobs from "./components/AppliedJobs/AppliedJobs";
+import Blog from "./components/Blog/Blog";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main></Main>,
+    children: [
+      { path: "/", element: <Home></Home> },
+      { path: "/statistics", element: <Statistics></Statistics> },
+      { path: "/appliedJobs", element: <AppliedJobs></AppliedJobs> },
+      { path: "/blog", element: <Blog></Blog> },
+    ],
   },
 ]);
 
